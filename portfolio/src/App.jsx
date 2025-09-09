119258be850c1d5ca45fbf729df0de3fbbf20c9a
HaydarChan <atqiyahaydar15@gmail.com>
2025-09-09 12:38:14 +0700

    feat: add font

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
+    <div className='text-[#1C1C1C]'>
+      <p className='text-[#1C1C1C]'>Hello</p>
       Hello
     </div>
   )
